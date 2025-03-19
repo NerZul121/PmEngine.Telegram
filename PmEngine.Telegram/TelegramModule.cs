a85d81d4bb3e45fcc5e97d14929d5e23cce44121
NerZul121 <NerZul121@mail.ru>
2025-03-19 17:15:09 +0300

    EnableRecive

--- PmEngine.Telegram/TelegramModule.cs
+++ PmEngine.Telegram/TelegramModule.cs
@@ -6,6 +6,7 @@ using PmEngine.Core.BaseClasses;
 using PmEngine.Core.Interfaces;
 using PmEngine.Telegram.Extensions;
 using PmEngine.Telegram.Interfaces;
+using System.Text.Json;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -45,6 +46,29 @@ namespace PmEngine.Telegram
             var guid = Guid.NewGuid();
             app.MapPost("/bot" + guid, ([FromBody] Update update) => botController.Post(update, app.Services));
             await tgClient.SetWebhook($"{Environment.GetEnvironmentVariable("HOST_URL")}/bot" + guid);
+        }
+
+        public static void EnableRecive(this WebApplication app, BaseTGController? controller = null)
+        {
+            EnableRecive(app.Services);
+        }
+
+        public static void EnableRecive(this IServiceProvider services, BaseTGController? controller = null)
+        {
+            if (controller is null)
+                controller = new();
+
+            var bot = services.GetRequiredService<ITelegramBotClient>();
+
+            bot.StartReceiving(async (bot, update, token) =>
+            {
+                Console.WriteLine(JsonSerializer.Serialize(update));
+                await controller.Post(update, services);
+            },
+            async (bot, exception, errorSource, token) =>
+            {
+                Console.WriteLine(exception);
+            });
         }
     }
 
